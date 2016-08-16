8c7da0bacaf984e010009ab64d0cd5d2994a9eec
salles-tristan <sallestaing@gmail.com>
2016-08-16 17:25:35 +1000

    add f2py function for swan

--- Model/libUtils/WaveComp/SimWaves.f90
+++ Model/libUtils/WaveComp/SimWaves.f90
@@ -1,35 +1,13 @@
-! =====================================================================================
-! ASCIDIAN
-!
-! Copyright (c) Tristan Salles (The University of Sydney)
-!
-! This program is free software; you can redistribute it and/or modify it under
-! the terms of the GNU Lesser General Public License as published by the Free Software
-! Foundation; either version 3.0 of the License,or (at your option) any later
-! version.
-!
-! This program is distributed in the hope that it will be useful,but WITHOUT
-! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-! FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-! more details.
-!
-! You should have received a copy of the GNU Lesser General Public License along with
-! this program; if not,write to the Free Software Foundation,Inc.,59 Temple
-! Place,Suite 330,Boston,MA 02111-1307 USA
-! =====================================================================================
-! =====================================================================================
-!
-!       Filename:  SimWaves.f90
-!
-!    Description:  Computes wave field.
-!
-!        Version:  1.0
-!        Created:  24/09/15 07:42:49
-!        Revision:  none
-!
-!        Author:  Tristan Salles
-!
-! =====================================================================================
+!!~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~!!
+!!                                                                                   !!
+!!  This file forms part of the pyReef carbonate platform modelling application.     !!
+!!                                                                                   !!
+!!  For full license and copyright information, please refer to the LICENSE.md file  !!
+!!  located at the project root, or contact the authors.                             !!
+!!                                                                                   !!
+!!~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~!!
+
+! Call to SWAN wave modelling model.
 module SimWaves
 
     !use hdf5
---
+++ Model/libUtils/simswan.f90
@@ -0,0 +1,85 @@
+!!~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~!!
+!!                                                                                   !!
+!!  This file forms part of the pyReef carbonate platform modelling application.     !!
+!!                                                                                   !!
+!!  For full license and copyright information, please refer to the LICENSE.md file  !!
+!!  located at the project root, or contact the authors.                             !!
+!!                                                                                   !!
+!!~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~!!
+
+! Main entry to SWAN wave modelling code.
+
+module simswan
+
+  use mpidata
+  use wavegrid
+  use SimWaves
+  use classdata
+  use miscdata
+
+  implicit none
+
+contains
+
+  subroutine run(comm)
+
+      real(kind=8)::time_st,time_ed
+      integer :: comm
+
+      ! start up MPI
+      !call mpi_init(ierr)
+      call mpi_comm_size(comm,nprocs,ierr)
+      call mpi_comm_rank(comm,iam,ierr)
+      ocean_comm_world=comm
+
+      int_type=mpi_integer
+      real_type=mpi_real
+      dbl_type=mpi_double_precision
+      lgc_type=mpi_logical
+      max_type=mpi_max
+      min_type=mpi_min
+      sum_type=mpi_sum
+
+      time_st=mpi_wtime( )
+
+      !--
+      ! Required data
+      xyzfile='data/gab_topo.nodes'
+      sp_n = 250
+      sp_m = 249
+      stratal_dx = 3000
+      wave_base = 100.0
+      !--
+      hindcast%wvel=30.0
+      hindcast%wdir=140.0
+      hindcast%hs=0.0_8
+      hindcast%per=0.0_8
+      hindcast%dir=0.0_8
+      hindcast%dd=0.0_8
+      !--
+      outdir='GABcirc'
+      !--
+
+      ! Initialisation step for several calls
+      ! Initialise swan model dataset and directory
+      call create_swan_data
+      call build_swan_model
+
+      ! Compute the wave fields for each conditions:
+      ! needs to update forecast based on next forecast_param for next run
+      ! needs to update topo sp_topo sea_level
+      call run_waves
+
+      time_ed=mpi_wtime( )
+      if(iam==0) print*,'Time elapse:',time_ed-time_st
+
+
+      ! Finalisation
+      ! Finalisation
+      call wave_final
+
+      return
+
+  end subroutine run
+
+end module simswan
